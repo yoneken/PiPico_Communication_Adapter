4977ce0e95a282e982081281a09734da3768f086
yoneken <yoneken@ieee.org>
2025-06-05 23:23:00 +0900

    Add Can bus

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 #include "task.h"
 #include "Modbus.h"
 
+extern "C" {
+  #include "can2040.h"
+  #include "hardware/irq.h"
+}
+
+#define MODBUS_TX_PIN 4
+#define MODBUS_RX_PIN 5
+#define MODBUS_EN_PIN 3
+#define CANBUS_RX_PIN 6
+#define CANBUS_TX_PIN 7
 
 modbusHandler_t ModbusH;
 uint16_t ModbusDATA[0x8ff];
@@ -78,6 +88,18 @@ Fn806, rated voltage, 1-6000, 0.01V, 2400, effective after power on, 0x0806
 Fn807, rated current, 1-2400, 0.01A, 800, effective after power on, 0x0807
 */
 
+static struct can2040 cbus;
+
+static void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
+{
+    printf("Core %u: Can Receive ID %d :(%d) %d\n", get_core_num(), (msg->id >> 7) & 0xF, (msg->id >> 1) & 0x3F, msg->dlc);
+}
+
+static void PIOx_IRQHandler(void)
+{
+    can2040_pio_irq_handler(&cbus);
+}
+
 void vTaskModbusSlave( void * pvParameters )
 {
     for(;;)
@@ -89,18 +111,84 @@ void vTaskModbusSlave( void * pvParameters )
     }
 }
 
+SemaphoreHandle_t CanTxSphrHandle = NULL;
 
-#define UART1_TX_PIN 4
-#define UART1_RX_PIN 5
+void vTaskCanTransmit( void * pvParameters )
+{
+    can2040_msg send_respond_encoder_data_msg;
+    send_respond_encoder_data_msg.id = (0 << 7) | (61 << 1); // 0 is the slave address, 61 is the function code
+    send_respond_encoder_data_msg.dlc = 5; // Data Length Code
+    send_respond_encoder_data_msg.data[0] = 0x0A;
+    send_respond_encoder_data_msg.data[1] = 0x14;
+    send_respond_encoder_data_msg.data[2] = 0x01;
+    send_respond_encoder_data_msg.data[3] = 0xF4;
+    send_respond_encoder_data_msg.data[4] = 0xC0;
+    int result = 0;
+    for(;;)
+    {
+        if(xSemaphoreTake(CanTxSphrHandle , portMAX_DELAY) == pdTRUE){
+            switch (flag) {
+                case 0:
+                    // Calibration
+                    send_respond_encoder_data_msg.id = (0 << 7) | (62 << 1);
+                    send_respond_encoder_data_msg.dlc = 0;
+                    flag = 1;
+                    break;
+                case 1:
+                    send_respond_encoder_data_msg.id = (0 << 7) | (61 << 1);
+                    send_respond_encoder_data_msg.dlc = 5;
+                    send_respond_encoder_data_msg.data[0] = 0x64;
+                    flag = 2;
+                    break;
+                case 2:
+                    send_respond_encoder_data_msg.id = (0 << 7) | (61 << 1);
+                    send_respond_encoder_data_msg.dlc = 5;
+                    send_respond_encoder_data_msg.data[0] = 0x0A;
+                    flag = 3;
+                    break;
+                case 3:
+                    send_respond_encoder_data_msg.id = (0 << 7) | (28 << 1) | CAN2040_ID_RTR;
+                    send_respond_encoder_data_msg.dlc = 0;
+                    flag = 0;
+                    break;
+            }
+            result = can2040_transmit(&cbus, &send_respond_encoder_data_msg);
+            printf("Core %u: CanTransmit %d %d\n", get_core_num(), result, counter++);
+            xSemaphoreGive(CanTxSphrHandle);
+        }
+        vTaskDelay(10);
+    }
+}
+
 
 
 void initSerial()
 {
     // Modbus Serial Port
     uart_init(uart1, 100000);
-    gpio_set_function(UART1_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART1_RX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(MODBUS_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(MODBUS_RX_PIN, GPIO_FUNC_UART);
     uart_set_fifo_enabled(uart1, false);
+}
+
+
+void initCanbus(void)
+{
+    uint32_t pio_num = 0;
+    uint32_t sys_clock = 125000000, bitrate = 1000000;
+    uint32_t gpio_rx = CANBUS_RX_PIN, gpio_tx = CANBUS_TX_PIN;
+
+    // Setup canbus
+    can2040_setup(&cbus, pio_num);
+    can2040_callback_config(&cbus, can2040_cb);
+
+    // Enable irqs
+    irq_set_exclusive_handler(PIO0_IRQ_0, PIOx_IRQHandler);
+    irq_set_priority(PIO0_IRQ_0, 1);
+    irq_set_enabled(PIO0_IRQ_0, 1);
+
+    // Start canbus
+    can2040_start(&cbus, sys_clock, bitrate, gpio_rx, gpio_tx);
 }
 
 
@@ -109,9 +197,10 @@ int main()
     stdio_init_all();
 
     initSerial();
+    initCanbus();
 
-    BaseType_t xReturnedModbusSlave;
-    TaskHandle_t xHandleModbusSlave = NULL;
+    BaseType_t xReturnedModbusSlave, xReturnedCanTransmit;
+    TaskHandle_t xHandleModbusSlave = NULL, xHandleCanTransmit = NULL;
     UBaseType_t uxCoreAffinityMask;
 
     xReturnedModbusSlave = xTaskCreate(
@@ -123,7 +212,7 @@ int main()
                     &xHandleModbusSlave );
 
 
-    // force to run Slave task on core1
+    // force to run Modbus Slave task on core1
     uxCoreAffinityMask = ( ( 1 << 1 ));
     vTaskCoreAffinitySet( xHandleModbusSlave, uxCoreAffinityMask );
 
@@ -133,7 +222,7 @@ int main()
     ModbusH.u16timeOut = 1000;
     // ModbusH.EN_Port = NULL;
     ModbusH.EN_Port = (uint16_t *) 1; //enables the RS485 ChipSelect
-    ModbusH.EN_Pin = 3; //Pi controlling RS485 ChipSelect
+    ModbusH.EN_Pin = MODBUS_EN_PIN; //Pi controlling RS485 ChipSelect
     ModbusH.u16regs = ModbusDATA;
     ModbusH.u16regsize= sizeof(ModbusDATA)/sizeof(ModbusDATA[0]);
     ModbusH.xTypeHW = USART_HW;
@@ -141,6 +230,18 @@ int main()
     ModbusInit(&ModbusH);
     //Start capturing traffic on serial Port
     ModbusStart(&ModbusH);
+
+    xReturnedCanTransmit = xTaskCreate(
+                    vTaskCanTransmit,
+                    "Can Transmit task",
+                    512,             /* Stack size in words, not bytes. */
+                    ( void * ) 1,    /* Parameter passed into the task. */
+                    tskIDLE_PRIORITY,/* Priority at which the task is created. */
+                    &xHandleCanTransmit );
+
+    CanTxSphrHandle = xSemaphoreCreateBinary();
+    xSemaphoreGive(CanTxSphrHandle);
+
 
     vTaskStartScheduler();
 
